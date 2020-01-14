d2b62b11703c8ff03b34747e102200c1fd4fa2d9
quang30091997 <quangbg997@gmail.com>
2020-01-14 10:35:20 +0700

    listProduct

--- model/Product/ProductDB.php
+++ model/Product/ProductDB.php
@@ -11,4 +11,18 @@ class ProductDB
         $db = new DBConnect();
         $this->conn = $db->connect();
     }
+
+    public function getAll()
+    {
+        $sql = "SELECT * FROM products";
+        $stmt = $this->conn->query($sql);
+        $result = $stmt->fetchAll();
+        $arr = [];
+        foreach ($result as $item) {
+            $product = new Product($item['name'], $item['price'], $item['quantity'], $item['description'], $item['img'], $item['category']);
+            array_push($arr, $product);
+            $product->setProductId($item['id']);
+        }
+        return $arr;
+    }
 }
