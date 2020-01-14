4bf13e41cdb7c3754d21645d531feb96d58fef05
minhcuong5920 <vuminhcuong5920@gmail.com>
2020-01-14 10:51:21 +0700

    search

--- model/Product/ProductDB.php
+++ model/Product/ProductDB.php
@@ -11,4 +11,14 @@ class ProductDB
         $db = new DBConnect();
         $this->conn = $db->connect();
     }
+
+    public function searchProduct($keyword)
+    {
+        $sql = "SELECT * FROM products p
+        INNER JOIN categories c
+        ON p.category = c.category_id
+        WHERE p.name LIKE '%$keyword%' OR c.name LIKE '%$keyword%'";
+        $stmt = $this->conn->query($sql);
+        $stmt->fetchAll();
+    }
 }
